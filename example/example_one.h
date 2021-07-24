1979548d57a32e31ab1047a105365881dbd4e1ac
CuntBoy <477740261@qq.com>
2021-07-25 01:42:59 +0800

    add new example to draw color map

---
+++ example/example_one.h
@@ -0,0 +1,17 @@
+#include <QWidget>
+#include <colormap/colormap.h>
+#include <colormap/colormapdata.h>
+
+std::shared_ptr<ColorMap> example_one()
+{
+    // 创建窗口对象
+    std::shared_ptr<ColorMap> widget(new ColorMap);
+
+    // 创建绘图的数据
+    std::shared_ptr<ColorMapData> data(new ColorMapData);
+
+    // 将数据设置给OPenGL
+    widget->colormap(data);
+
+    return widget;
+}
